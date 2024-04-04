288bae74846bd709b0816741519660fcc2b1ab6b
Octavyan Putra Ramadhan <octavyan4@gmail.com>
2024-04-05 02:55:05 +0700

    FIX: add avatar data in user resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -16,6 +16,7 @@ class UserResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'avatar' => $this->avatar,
             'username' => $this->username,
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
